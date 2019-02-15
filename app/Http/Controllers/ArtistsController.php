6779f2f51c980c749eb1d26596ba984f448c67f2
nicrausaz <n.crausaz99@gmail.com>
2019-02-15 20:21:50 +0100

    pagination working, need style

--- app/Http/Controllers/ArtistsController.php
+++ app/Http/Controllers/ArtistsController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Storage;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Request;
 
 class ArtistsController extends Controller {
 
+    private $request;
     private $musicPath = '\\\\NAS-CRAUSAZ\\music\\';
     private $artists = [];
+
+    public function __construct (Request $request) {
+        $this->request = $request;
+    }
 
     public function getAll () {
         $items = array_diff(scandir($this->musicPath), array('.', '..'));;
@@ -18,7 +26,16 @@ class ArtistsController extends Controller {
             }
         }
 
-        return view('artists', ['artists' => $this->artists]);
+        $selected_page = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 30;
+        $current_page_items = array_slice($items, $perPage * ($selected_page - 1), $perPage);
+
+
+        $paginated_artists = new LengthAwarePaginator($current_page_items, count($items), $perPage, $selected_page);
+
+        $paginated_artists->withPath('artists');
+
+        return view('artists', ['artists' => $paginated_artists]);
     }
 
     private function getOne () {
--- resources/views/artists.blade.php
+++ resources/views/artists.blade.php
@@ -5,10 +5,14 @@
 @section('content')
 <div class="container">
     <div class="notification">
+        <div class="control is-loading">
+            <input class="input" type="search" placeholder="Search...">
+        </div>
         {{count($artists)}}
         @foreach ($artists as $artist)
             <p>{{ $artist }}</p>
         @endforeach
+        {{$artists->links()}}
     </div>
 </div>
 @endsection
